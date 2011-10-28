21daee7076b83ba508a5d3a2e929375d8299fc53
Firas Zaidan <firas.zaidan@seonic.net>
2011-10-28 19:08:38 +0200

    improve rolling statistics

--- lib/rz/service/statistics.rb
+++ lib/rz/service/statistics.rb
@@ -3,45 +3,45 @@ module RZ
     module Statistics
       def before_loop
         @s_loops = @s_noops = @s_requests = @s_responses = 0
-        @s_loop_start_time ||= Time.now
-        reset_short_stats
+        @s_loop_start_time = Time.now
+        @history = []
+        self
       end
 
       def loop_end
-        reset_short_stats if (@s_loops % 100).zero?
-        print_stats
+        now = Time.now
+        messages = @s_requests + @s_responses
+        ms = messages / (now - @s_loop_start_time)
+        @history << [now,messages]
+
+        old_time,old_messages = @history.length == 1000 ? @history.slice!(0) : @history.first
+
+        mss = now == old_time ? 0 : (messages - old_messages) / (now - old_time)
+
+        $stderr.puts "msg: req: %03d rep: %03d noop: %03d miss: %03d m/s: %0.2f ms/s: %0.2f" % [@s_requests,@s_responses,@s_noops,@s_requests - @s_requests,ms,mss]
+        self
       end
 
       attr_reader :short_message_count,:short_time
 
-      def reset_short_stats
-        @s_short_time = Time.now
-        @s_short_message_count = @s_requests + @s_responses
-      end
-
-      def print_stats
-        messages = @s_requests + @s_responses
-        ms = messages / (Time.now - @s_loop_start_time)
-        short_messages = messages - @s_short_message_count
-        mss = short_messages / (Time.now - @s_short_time)
-        $stderr.puts "msg: req: %03d rep: %03d noop: %03d miss: %03d m/s: %0.2f ms/s: %0.2f" % [@s_requests,@s_responses,@s_noops,@s_requests - @s_requests,ms,mss]
-        self
-      end
-     
       def noop
         @s_noops +=1
+        self
       end
 
       def request
         @s_requests +=1
+        self
       end
 
       def response
         @s_responses +=1
+        self
       end
 
       def loop_start
         @s_loops += 1
+        self
       end
 
       def self.included(base)
