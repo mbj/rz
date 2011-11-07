f86cb676d9665ad2c43ab020f4217343afc0a985
Markus Schirp <mbj@seonic.net>
2011-11-07 23:40:16 +0100

    fix statistic miss bug

--- lib/rz/service/statistics.rb
+++ lib/rz/service/statistics.rb
@@ -18,7 +18,7 @@ module RZ
 
         mss = now == old_time ? 0 : (messages - old_messages) / (now - old_time)
 
-        $stderr.puts "msg: req: %03d rep: %03d noop: %03d miss: %03d m/s: %0.2f ms/s: %0.2f" % [@s_requests,@s_responses,@s_noops,@s_requests - @s_requests,ms,mss]
+        $stderr.puts "msg: req: %03d rep: %03d noop: %03d miss: %03d m/s: %0.2f ms/s: %0.2f" % [@s_requests,@s_responses,@s_noops,@s_requests - @s_responses,ms,mss]
         self
       end
 
